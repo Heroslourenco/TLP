35d9aa2274d28500f28acd1449e4fd663f4dd4e2
Heroslourenco <heroslourenco710@gmail.com>
2024-11-21 14:16:25 +0000

    Add files via upload

---
+++ EX8.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<time.h>
+#include <cstdlib>
+void main() {
+	using namespace std;
+	int serteio[5], aposta[5], i, j , acertou;
+	string sorteio[5];
+		srand(time(0));
+		for (i = 0; i < 5; i++)
+		{
+			sorteio[i] = rand() % 50 + 1;
+			cout << sorteio[5] << "\n";
+
+
+
+
+				cin >> aposta[5];
+
+		}
+		
+		{	
+			if(sorteio == aposta);
+			
+		}
+}
+
