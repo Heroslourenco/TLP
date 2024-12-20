2bd558044d8b925a9d22e105201cb9b3222e97fc
Heroslourenco <heroslourenco710@gmail.com>
2024-12-20 16:29:11 +0000

    Add files via upload

---
+++ teste.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+void main() {
+	int i,j, contarP=0,tamanho;
+	char palavra, vogais[5] = {'a','e','i','o','u'};
+	cout << "diz o tamanho";
+	cin >> tamanho;
+	cout << "diz me a palavra";
+	cin >> palavra;
+	for ( i = 0; i < tamanho; i++)
+	{
+		if ((palavra == contarP) || ( palavra == contarP))
+		{
+			vogais[i] = contarP + 1 ;
+			
+		}
+		cout << vogais << contarP;
+	}
+
+
+
+
+
+
+
+}
